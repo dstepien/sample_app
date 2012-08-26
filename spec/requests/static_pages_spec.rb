ccc662f6873fe0e5cc6bfa763566ad5d6b70edd1
Dawid Stępień <kontakt@dawidstepien.pl>
2012-08-26 17:16:21 +0200

    Add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,4 +33,15 @@ describe "Static pages" do
       page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About')
     end
   end
+
+  describe "Contact page" do
+    it "should have content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => 'Contact')
+    end
+  end
 end
